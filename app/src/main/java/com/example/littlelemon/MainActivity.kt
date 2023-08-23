8afb6fecd385844e48101002bb616ee57ad4c446
weyyam <williammichaelmcmahon@gmail.com>
2023-08-22 22:35:53 -0400

    Almost done first page, still working on getting the inputs to save their inputs not entirely sure what im supposed to save it to but i guess ill figure theat out. Also trying to make it look a little better, not entirely sure ive dont the inputs properly but im sure i could look at a courseear video to show me. this is the las of the onboard and should be able to get it done enough soon, making it look nice and cool might take some time but priority is to get it working

--- app/src/main/java/com/example/littlelemon/MainActivity.kt
+++ app/src/main/java/com/example/littlelemon/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.littlelemon.ui.theme.LittleLemonTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,8 +20,7 @@ class MainActivity : ComponentActivity() {
             LittleLemonTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+
                 ) {
                     Onboarding()
                 }
@@ -33,7 +33,7 @@ class MainActivity : ComponentActivity() {
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
         text = "Hello $name!",
-        modifier = modifier
+
     )
 }
 
--- app/src/main/java/com/example/littlelemon/Onboarding.kt
+++ app/src/main/java/com/example/littlelemon/Onboarding.kt
@@ -1,48 +1,103 @@
 package com.example.littlelemon
 
-import android.provider.CalendarContract.Colors
-import android.widget.HeaderViewListAdapter
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.littlelemon.ui.theme.LittleLemonTheme
 
 class OnBoarding {
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Onboarding (){
-    Box(
-        modifier = Modifier
-            .fillMaxWidth()
-            .height(2.dp)
-            .background(colorResource(id = R.color.white))
-            .padding(top = 10.dp),
-    contentAlignment = Alignment.TopCenter
-    ){
-        Image(
-            painter = painterResource(id = R.drawable.logo),
-            contentDescription = "Logo Header image",
-            contentScale = ContentScale.Crop,
+
+    var firstname by remember {
+        mutableStateOf(TextFieldValue(""))
+    }
+    var lastname by remember {
+        mutableStateOf(TextFieldValue(""))
+    }
+    var emailaddress by remember {
+        mutableStateOf(TextFieldValue(""))
+    }
+
+    Column(modifier = Modifier.background(color = colorResource(id = R.color.white))) {
+        Box(
             modifier = Modifier
                 .fillMaxWidth()
+                .height(100.dp)
+                .background(colorResource(id = R.color.white)),
+            contentAlignment = Alignment.TopCenter
+        ){
+            Image(
+                painter = painterResource(id = R.drawable.logo),
+                contentDescription = "Logo Header image",
+                contentScale = ContentScale.Fit,
+                modifier = Modifier.size(250.dp)
+            )
+        }
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(150.dp)
+                .background(colorResource(id = R.color.green))
+        ){
+            Text(
+                text = "Let's get to know you!",
+                fontSize = 24.sp,
+                modifier = Modifier.align(Alignment.Center)
+            )
+        }
+
+        Text(text = "Personal Information", color = colorResource(id = R.color.black), modifier = Modifier.padding(horizontal = 10.dp, vertical = 50.dp))
+
+        Box(modifier = Modifier
+            .fillMaxSize()
+            .background(colorResource(id = R.color.white))
         )
+        {
+            Column(modifier = Modifier.padding(10.dp)) {
+                TextField(value = firstname, onValueChange = {firstname = it}, label = { Text(text = "First Name")}, modifier = Modifier.padding(10.dp))
+                TextField(value = lastname, onValueChange = {lastname = it}, label = { Text(text = "Last Name")}, modifier = Modifier.padding(10.dp))
+                TextField(value = emailaddress, onValueChange = {emailaddress = it}, label = { Text(text = "Email Address")}, modifier = Modifier.padding(10.dp))
+                
+                Button(onClick = { /*TODO*/ },modifier = Modifier.padding(10.dp)){
+                    Text(text = "Register")
+                }
+            }
+
+        }
     }
+
+
 }
 
 @Preview(showBackground = true)
