723719a7c6c03efae37ebf8c96b9270a78ee7ddd
weyyam <williammichaelmcmahon@gmail.com>
2023-08-23 20:51:42 -0400

    Onboard pretty much done from a looks stand point, still unsure if it is functional but thats the next issue

--- app/src/main/java/com/example/littlelemon/Onboarding.kt
+++ app/src/main/java/com/example/littlelemon/Onboarding.kt
@@ -10,10 +10,17 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.selection.TextSelectionColors
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldColors
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -25,12 +32,18 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.littlelemon.ui.theme.LittleLemonTheme
+import com.example.littlelemon.ui.theme.MyTypography
+import com.example.littlelemon.ui.theme.charcoal
+import com.example.littlelemon.ui.theme.cloud
+import com.example.littlelemon.ui.theme.yellow
+import kotlin.math.round
 
 class OnBoarding {
 }
@@ -49,7 +62,11 @@ fun Onboarding (){
         mutableStateOf(TextFieldValue(""))
     }
 
-    Column(modifier = Modifier.background(color = colorResource(id = R.color.white))) {
+    val textFieldColorsForInput = TextFieldDefaults.outlinedTextFieldColors(textColor = charcoal, containerColor = cloud)
+
+    Column(modifier = Modifier
+        .background(color = colorResource(id = R.color.white))
+        .fillMaxHeight()) {
         Box(
             modifier = Modifier
                 .fillMaxWidth()
@@ -73,28 +90,65 @@ fun Onboarding (){
             Text(
                 text = "Let's get to know you!",
                 fontSize = 24.sp,
-                modifier = Modifier.align(Alignment.Center)
+                modifier = Modifier.align(Alignment.Center),
+                style = MyTypography.titleLarge
             )
         }
 
-        Text(text = "Personal Information", color = colorResource(id = R.color.black), modifier = Modifier.padding(horizontal = 10.dp, vertical = 50.dp))
-
-        Box(modifier = Modifier
-            .fillMaxSize()
-            .background(colorResource(id = R.color.white))
+        Text(
+            text = "Personal Information",
+            color = colorResource(id = R.color.black),
+            modifier = Modifier.padding(horizontal = 10.dp, vertical = 50.dp),
+            style = MyTypography.bodyLarge
         )
-        {
-            Column(modifier = Modifier.padding(10.dp)) {
-                TextField(value = firstname, onValueChange = {firstname = it}, label = { Text(text = "First Name")}, modifier = Modifier.padding(10.dp))
-                TextField(value = lastname, onValueChange = {lastname = it}, label = { Text(text = "Last Name")}, modifier = Modifier.padding(10.dp))
-                TextField(value = emailaddress, onValueChange = {emailaddress = it}, label = { Text(text = "Email Address")}, modifier = Modifier.padding(10.dp))
-                
-                Button(onClick = { /*TODO*/ },modifier = Modifier.padding(10.dp)){
-                    Text(text = "Register")
-                }
-            }
 
+        Column(modifier = Modifier
+            .padding(10.dp)
+            .fillMaxWidth()) {
+            OutlinedTextField(
+                value = firstname,
+                onValueChange = {firstname = it},
+                placeholder = { Text(text = "First Name")},
+                modifier = Modifier
+                    .padding(10.dp)
+                    .fillMaxWidth(),
+                shape = RoundedCornerShape(8.dp),
+                colors = textFieldColorsForInput
+
+            )
+            OutlinedTextField(
+                value = lastname,
+                onValueChange = {lastname = it},
+                label = { Text(text = "Last Name")},
+                modifier = Modifier
+                    .padding(10.dp)
+                    .fillMaxWidth(),
+                shape = RoundedCornerShape(8.dp),
+                colors = textFieldColorsForInput)
+            OutlinedTextField(
+                value = emailaddress,
+                onValueChange = {emailaddress = it},
+                label = { Text(text = "Email Address")},
+                modifier = Modifier
+                    .padding(10.dp)
+                    .fillMaxWidth(),
+                shape = RoundedCornerShape(8.dp),
+                colors = textFieldColorsForInput)
+
+            Button(onClick = { /*TODO*/ },modifier = Modifier
+                .padding(10.dp)
+                .align(Alignment.CenterHorizontally)
+                .fillMaxWidth(),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = yellow ,
+                    contentColor = charcoal
+                )
+            ){
+                Text(text = "Register")
+            }
         }
+
+
     }
 
 
--- app/src/main/java/com/example/littlelemon/ui/theme/Type.kt
+++ app/src/main/java/com/example/littlelemon/ui/theme/Type.kt
@@ -2,19 +2,38 @@ package com.example.littlelemon.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 
 // Set of Material typography styles to start with
-val Typography = Typography(
+val MyTypography = Typography(
+    titleLarge = TextStyle(
+      fontSize = 26.sp,
+        color = cloud
+    ),
+    displayLarge = TextStyle(
+        fontSize = 26.sp,
+        fontWeight = FontWeight.Bold,
+        color = charcoal
+    ),
+    displayMedium = TextStyle(
+        color = charcoal,
+        fontSize = 18.sp,
+        fontWeight = FontWeight.Bold
+    ),
+    bodyMedium = TextStyle(
+        color = green
+    ),
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
+        fontWeight = FontWeight.Bold,
+        color = green,
+        fontSize = 16.sp
+    ),
+    labelMedium = TextStyle(
+        fontSize = 14.sp,
+        fontWeight = FontWeight.Bold
     )
+
     /* Other default text styles to override
     titleLarge = TextStyle(
         fontFamily = FontFamily.Default,
@@ -32,3 +51,4 @@ val Typography = Typography(
     )
     */
 )
+
