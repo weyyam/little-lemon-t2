23e960c569c2e70f62f31df2c493d61140d3eae5
weyyam <williammichaelmcmahon@gmail.com>
2023-08-24 18:22:18 -0400

    Need to fix profilepage home button but appart from that the profile page looks good

--- app/src/main/java/com/example/littlelemon/Profile.kt
+++ app/src/main/java/com/example/littlelemon/Profile.kt
@@ -1,32 +1,134 @@
 package com.example.littlelemon
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
+import com.example.littlelemon.ui.theme.MyTypography
+import com.example.littlelemon.ui.theme.charcoal
+import com.example.littlelemon.ui.theme.cloud
+import com.example.littlelemon.ui.theme.yellow
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ProfileScreen(navController: NavHostController) {
     val context = LocalContext.current
     var savedFirstName = PreferenceHelper.getFirstName(context)
+    var savedLastName = PreferenceHelper.getLastName(context)
+    var savedEmailaddress = PreferenceHelper.getEmailAddress(context)
+
+
     Column(
-        modifier = Modifier.fillMaxSize()
-    ){
-        Text(text = "ProfileScreen")
-        Button(
-            onClick = {
-                navController.navigate(Home.route)
-            }) {
-            Text(text = "BackButton")
+        modifier = Modifier
+            .background(color = cloud)
+            .fillMaxHeight()
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(100.dp)
+                .background(color = cloud),
+            contentAlignment = Alignment.TopCenter
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.logo),
+                contentDescription = "Logo Header image",
+                contentScale = ContentScale.Fit,
+                modifier = Modifier.size(250.dp)
+            )
         }
-        TextField(value = savedFirstName, onValueChange = { savedFirstName = it })
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(150.dp)
+                .background(colorResource(id = R.color.green))
+        ) {
+            Text(
+                text = "Edit Profile!",
+                fontSize = 24.sp,
+                modifier = Modifier.align(Alignment.Center),
+                style = MyTypography.titleLarge
+            )
+        }
+
+        Text(
+            text = "Personal Information",
+            color = colorResource(id = R.color.black),
+            modifier = Modifier.padding(horizontal = 10.dp, vertical = 50.dp),
+            style = MyTypography.bodyLarge
+        )
+
+        Column(
+            modifier = Modifier
+                .padding(10.dp)
+                .fillMaxWidth()
+        ) {
+            OutlinedTextField(
+                value = savedFirstName,
+                onValueChange = { savedFirstName = it },
+                placeholder = { Text(text = "First Name") },
+                modifier = Modifier
+                    .padding(10.dp)
+                    .fillMaxWidth(),
+                shape = RoundedCornerShape(8.dp),
+                singleLine = true
+
+            )
+            OutlinedTextField(
+                value = savedLastName,
+                onValueChange = { savedLastName = it },
+                label = { Text(text = "Last Name") },
+                modifier = Modifier
+                    .padding(10.dp)
+                    .fillMaxWidth(),
+                shape = RoundedCornerShape(8.dp),
+                singleLine = true
+            )
+            OutlinedTextField(
+                value = savedEmailaddress,
+                onValueChange = { savedEmailaddress = it },
+                label = { Text(text = "Email Address") },
+                modifier = Modifier
+                    .padding(10.dp)
+                    .fillMaxWidth(),
+                shape = RoundedCornerShape(8.dp),
+                singleLine = true
+            )
+        }
+    }
+
+    Button(
+        onClick = {
+            navController.navigate(Home.route)
+        }
+        ,modifier = Modifier
+            .padding(10.dp)
+            .fillMaxWidth(),
+        colors = ButtonDefaults.buttonColors(
+            containerColor = yellow ,
+            contentColor = charcoal
+        )){
+        Text(text = "HomePage")
     }
 }
