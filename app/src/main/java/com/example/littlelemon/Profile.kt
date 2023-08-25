7a55630db1c49565ff42f6c48efe3e329c7ffb40
weyyam <williammichaelmcmahon@gmail.com>
2023-08-24 21:54:55 -0400

    Made the profile page look at little nicer, The signout button still does not work

--- app/src/main/java/com/example/littlelemon/Profile.kt
+++ app/src/main/java/com/example/littlelemon/Profile.kt
@@ -1,34 +1,47 @@
 package com.example.littlelemon
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.ShapeDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Constraints
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
 import com.example.littlelemon.ui.theme.MyTypography
 import com.example.littlelemon.ui.theme.charcoal
 import com.example.littlelemon.ui.theme.cloud
 import com.example.littlelemon.ui.theme.yellow
+import kotlin.math.round
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -44,19 +57,43 @@ fun ProfileScreen(navController: NavHostController) {
             .background(color = cloud)
             .fillMaxHeight()
     ) {
-        Box(
+        Row(
             modifier = Modifier
                 .fillMaxWidth()
                 .height(100.dp)
                 .background(color = cloud),
-            contentAlignment = Alignment.TopCenter
+            verticalAlignment = Alignment.CenterVertically
         ) {
+            Button(onClick = { /*TODO*/ },
+                colors = ButtonDefaults.buttonColors(yellow),
+                modifier = Modifier
+                    .padding(horizontal = 5.dp)
+                    .weight(0.5f)
+                ) {
+                Image(
+                    painter = painterResource(id = R.drawable.round_arrow_back_24),
+                    contentDescription = "Back button to get to home page")
+
+            }
+
             Image(
                 painter = painterResource(id = R.drawable.logo),
                 contentDescription = "Logo Header image",
                 contentScale = ContentScale.Fit,
-                modifier = Modifier.size(250.dp)
+                modifier = Modifier
+                    .padding(5.dp)
+                    .weight(1.25f)
+                    .fillMaxSize()
             )
+            Image(
+                painter = painterResource(id = R.drawable.profile),
+                contentDescription = "Profile photo",
+            modifier = Modifier
+                .padding(5.dp)
+                .weight(0.5f)
+                .clip(CircleShape))
+
+
         }
         Box(
             modifier = Modifier
@@ -71,64 +108,87 @@ fun ProfileScreen(navController: NavHostController) {
                 style = MyTypography.titleLarge
             )
         }
+        Column(modifier = Modifier.fillMaxSize()) {
+            Text(
+                text = "Personal Information",
+                color = colorResource(id = R.color.black),
+                modifier = Modifier.padding(horizontal = 10.dp, vertical = 50.dp),
+                style = MyTypography.bodyLarge
+            )
 
-        Text(
-            text = "Personal Information",
-            color = colorResource(id = R.color.black),
-            modifier = Modifier.padding(horizontal = 10.dp, vertical = 50.dp),
-            style = MyTypography.bodyLarge
-        )
-
-        Column(
-            modifier = Modifier
-                .padding(10.dp)
-                .fillMaxWidth()
-        ) {
-            OutlinedTextField(
-                value = savedFirstName,
-                onValueChange = { savedFirstName = it },
-                placeholder = { Text(text = "First Name") },
+            Column(
                 modifier = Modifier
                     .padding(10.dp)
-                    .fillMaxWidth(),
-                shape = RoundedCornerShape(8.dp),
-                singleLine = true
+                    .fillMaxWidth()
+            ) {
+                OutlinedTextField(
+                    value = savedFirstName,
+                    onValueChange = { savedFirstName = it },
+                    placeholder = { Text(text = "First Name") },
+                    modifier = Modifier
+                        .padding(10.dp)
+                        .fillMaxWidth(),
+                    shape = RoundedCornerShape(8.dp),
+                    singleLine = true
 
-            )
-            OutlinedTextField(
-                value = savedLastName,
-                onValueChange = { savedLastName = it },
-                label = { Text(text = "Last Name") },
-                modifier = Modifier
+                )
+                OutlinedTextField(
+                    value = savedLastName,
+                    onValueChange = { savedLastName = it },
+                    label = { Text(text = "Last Name") },
+                    modifier = Modifier
+                        .padding(10.dp)
+                        .fillMaxWidth(),
+                    shape = RoundedCornerShape(8.dp),
+                    singleLine = true
+                )
+                OutlinedTextField(
+                    value = savedEmailaddress,
+                    onValueChange = { savedEmailaddress = it },
+                    label = { Text(text = "Email Address") },
+                    modifier = Modifier
+                        .padding(10.dp)
+                        .fillMaxWidth(),
+                    shape = RoundedCornerShape(8.dp),
+                    singleLine = true
+                )
+            }
+            Button(onClick = { /*TODO*/ }
+                , modifier = Modifier
                     .padding(10.dp)
                     .fillMaxWidth(),
-                shape = RoundedCornerShape(8.dp),
-                singleLine = true
-            )
-            OutlinedTextField(
-                value = savedEmailaddress,
-                onValueChange = { savedEmailaddress = it },
-                label = { Text(text = "Email Address") },
-                modifier = Modifier
+                colors = ButtonDefaults
+                    .buttonColors(
+                        containerColor = yellow,
+                        contentColor = charcoal))
+            {
+                Text(text = "Sign Out")
+            }
+
+            Button(
+                onClick = {
+                    navController.navigate(Home.route)
+                }
+                ,modifier = Modifier
                     .padding(10.dp)
                     .fillMaxWidth(),
-                shape = RoundedCornerShape(8.dp),
-                singleLine = true
-            )
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = yellow ,
+                    contentColor = charcoal
+                )){
+                Text(text = "Home Page")
+            }
         }
+
     }
 
-    Button(
-        onClick = {
-            navController.navigate(Home.route)
-        }
-        ,modifier = Modifier
-            .padding(10.dp)
-            .fillMaxWidth(),
-        colors = ButtonDefaults.buttonColors(
-            containerColor = yellow ,
-            contentColor = charcoal
-        )){
-        Text(text = "HomePage")
-    }
+
 }
+/*
+@Preview(showBackground = true)
+@Composable
+fun ProfileScreenPreview(){
+    ProfileScreen(navController = rememberNavController())
+}
+
+ */
