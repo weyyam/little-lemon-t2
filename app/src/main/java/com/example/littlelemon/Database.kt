52d9e34db5a56d43bb5dd8eb5e3058ae3255ba3a
weyyam <williammichaelmcmahon@gmail.com>
2023-09-04 14:41:20 -0400

    Fixed error involving updating data

--- app/src/main/java/com/example/littlelemon/Database.kt
+++ app/src/main/java/com/example/littlelemon/Database.kt
@@ -5,6 +5,7 @@ import androidx.room.Dao
 import androidx.room.Database
 import androidx.room.Entity
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.PrimaryKey
 import androidx.room.Query
 import androidx.room.Room
@@ -26,7 +27,7 @@ interface MenuItemDao{
     @Query("SELECT * FROM menu_items")
     fun getAllMenuItems(): List<MenuItemEntity>
 
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun insertAll(vararg menuItems: MenuItemEntity)
 }
 
